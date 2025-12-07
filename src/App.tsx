0f232b8bdef19be2f6ec63dba50aa918291396a2
Ahmed Fathy <iahmed.fathy@hotmail.com>
2025-12-07 21:42:17 +0200

    implement shopping cart using Context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import ProductList from "./components/ProductList";
+import CartList from "./components/CartList/CartList";
+import ProductList from "./components/ProductList/ProductList";
 
 function App() {
   return (
-    <main>
+    <main className="grid grid-cols-[2fr_1fr] gap-4 min-h-screen m-10">
       <ProductList />
+      <CartList />
     </main>
   );
 }
