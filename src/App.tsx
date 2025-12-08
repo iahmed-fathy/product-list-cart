150040ac39ddf3c719360f6d12ed06dff8e3fd30
Ahmed Fathy <iahmed.fathy@hotmail.com>
2025-12-08 22:35:09 +0200

    complete cart and order confirmation dialog

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ProductList from "./components/ProductList/ProductList";
 
 function App() {
   return (
-    <main className="grid grid-cols-[2fr_1fr] gap-4 min-h-screen m-10">
+    <main className="grid grid-cols-[2fr_1fr] max-xl:grid-cols-1 gap-10 min-h-screen m-10">
       <ProductList />
       <CartList />
     </main>
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { DataProvider } from "./DataContext.tsx";
+import DataProviderContainer from "./store/DataProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <DataProvider>
+    <DataProviderContainer>
       <App />
-    </DataProvider>
+    </DataProviderContainer>
   </StrictMode>,
 );
